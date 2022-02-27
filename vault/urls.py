08b8cb13e56378ae7cfcc6f06722ca8a7b4cb230
Harry Potter <random@gmail.com>
2022-02-27 06:24:06 -0700

    update

--- vault/urls.py
+++ vault/urls.py
@@ -1,4 +1,4 @@
-"""3dFixed URL Configuration
+"""vault URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
